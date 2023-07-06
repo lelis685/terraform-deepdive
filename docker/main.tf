9ea26aac24c363cf6a26d5b8e0330137f30a82fb
lelis <lelis685@gmail.com>
2023-07-06 09:29:09 -0300

    config docker container

--- docker/main.tf
+++ docker/main.tf
@@ -1,4 +1,13 @@
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
+}
+
+resource "docker_container" "nodered_container" {
+  name  = "nodered"
+  image = docker_image.nodered_image.image_id
+  ports {
+    internal = 1880
+    external = 1880
+  }
 
 }
